25b6e2cf1448233fbefad8dc65c979308b5f9abf
Sergey Borisov <fllashbllack99@mail.ru>
2020-02-19 11:07:54 +0700

    Добавлен проект логики

--- NoteAppUI/NoteAppUI/Form2.cs
+++ NoteAppUI/NoteAppUI/Form2.cs
@@ -7,18 +7,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+using NoteApp;
 
 namespace NoteAppUI
 {
-    public partial class Form1 : Form
+    public partial class Form2 : Form
     {
-        public Form1()
+        public Form2()
         {
             InitializeComponent();
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form2_Load(object sender, EventArgs e)
         {
 
         }
